915fb93459095ccf9ed7d25d743c39d9a969acb9
dongxia.wdx <dongxia.wdx@alibaba-inc.com>
2019-09-11 23:21:26 +0800

    init commit

---
+++ app/src/main/java/personal/dongxia/android/multimeter/MainActivity.java
@@ -0,0 +1,14 @@
+package personal.dongxia.android.multimeter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
